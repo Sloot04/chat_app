9e3bd67af9657d264b90e3ce0fdd613aa93111b1
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-18 21:36:34 -0300

    LoginPage style

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/custom_logo.dart';
+import 'package:chat_app/widgets/custom_labels.dart';
+import 'package:chat_app/widgets/boton_azul.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
-     ),
-   );
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Logo(),
+            _Form(),
+            Labels(),
+            Text(
+              'Términos y condiciones de uso',
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form({Key? key}) : super(key: key);
+
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final TextEditingController emailCtrl = TextEditingController();
+  final TextEditingController passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            textController: emailCtrl,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passCtrl,
+            isPassword: true,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          BotonAzul(
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            }, text: 'Ingrese',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  const BotonAzul({required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          elevation: MaterialStateProperty.all(2),
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+          shape: MaterialStateProperty.all(StadiumBorder())),
+      onPressed: () => this.onPressed(),
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(
+            this.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
