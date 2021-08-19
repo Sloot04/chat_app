b8848c8deaa7f69b4e948c2f475effaf22555e03
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-18 22:27:12 -0300

    RegisterPage

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,14 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/custom_logo.dart';
+import 'package:chat_app/widgets/custom_labels.dart';
+import 'package:chat_app/widgets/boton_azul.dart';
 
 class RegisterPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Register Page'),
-     ),
-   );
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Logo(titulo: 'Registro',),
+                _Form(),
+                Labels(ruta: 'login', titulo: 'Ingresa ahora', subtitulo: '¿Ya tienes una cuenta?',),
+                Text(
+                  'Términos y condiciones de uso',
+                  style: TextStyle(fontWeight: FontWeight.w200),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form({Key? key}) : super(key: key);
+
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final TextEditingController nameCtrl = TextEditingController();
+  final TextEditingController emailCtrl = TextEditingController();
+  final TextEditingController passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.perm_identity,
+            placeholder: 'Nombre',
+            textController: nameCtrl,
+            keyboardType: TextInputType.text,
+          ),
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            textController: emailCtrl,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passCtrl,
+            isPassword: true,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          BotonAzul(
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+            text: 'Ingrese',
+          ),
+        ],
+      ),
+    );
   }
 }
